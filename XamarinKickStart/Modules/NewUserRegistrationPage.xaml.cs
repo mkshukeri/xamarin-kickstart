fb9f0ce7a872898ab75c9fc45e0bb9e4c62f7f7a
mnazrii <mnazrii@live.com>
2017-04-12 14:02:53 +0800

    plain registration
    plain new user registration page

---
+++ XamarinKickStart/Modules/NewUserRegistrationPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinKickStart
+{
+	public partial class NewUserRegistrationPage : ContentPage
+	{
+		public NewUserRegistrationPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
